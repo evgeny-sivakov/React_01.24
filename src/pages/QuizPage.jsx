853547540dbfea9a166041453c0d8835e0c22c88
Evgeny Sivakov <jsivakov@gmail.com>
2024-03-02 19:02:41 +0100

    homework_9

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -1,14 +1,22 @@
 import ContentCard from '../components/layouts/ContentCard'
 import Quiz from '../components/Quiz'
+import { AnimatePresence, motion } from 'framer-motion'
 
 const QuizPage = () => {
   return (
-    <section className="main-section">
-      <h1>Quiz Page!</h1>
-      <ContentCard>
-        <Quiz />
-      </ContentCard>
-    </section>
+    <AnimatePresence>
+      <motion.section
+        key="quizPage"
+        initial={{ x: 300, opacity: 0 }}
+        animate={{ x: 0, opacity: 1, transition: { duration: 0.5 } }}
+        exit={{ x: -300, opacity: 0 }}
+        className="main-section">
+        <h1>Quiz Page!</h1>
+        <ContentCard>
+          <Quiz />
+        </ContentCard>
+      </motion.section>
+    </AnimatePresence>
   )
 }
 
